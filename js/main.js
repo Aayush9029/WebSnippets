36c1edf171cfebe4748e1d2a868a11ede51bc260
Aayush <43297314+Aayush9029@users.noreply.github.com>
2022-07-25 03:55:19 +0000

    add view

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let darkTheme = false;
+
+const themeToggle = document.getElementById("toggleTheme");
+
+themeToggle.addEventListener("click", function(){
+    darkTheme = !darkTheme;
+    darkTheme ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
+    themeToggle.checked = darkTheme;
+});
+
