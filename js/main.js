ebcbac35011416b9f20a9f0d881aedf98af8d8bb
Aayush <43297314+Aayush9029@users.noreply.github.com>
2022-07-31 08:39:05 +0000

    UI changes

--- js/main.js
+++ js/main.js
@@ -10,9 +10,15 @@ const addNewSnippet = () => {
     // split the keywords into an array by comma
     const keywordsArray = keywords.value.trim().split(",");
     // remove whitespace from the keywords
-    const keywordsArrayClean = keywordsArray.map(keyword => keyword.trim());
-    // create a new snippet object
-
+    const keywordsArrayClean = keywordsArray.map((keyword) => {
+        // remove blank keywords
+        if (keyword.trim() === "") {
+            return null;
+        }
+        return keyword.trim();
+     }
+    );  
+        // create a new snippet object
     const newSnippet = {
         title: title.value,
         keywords: keywordsArrayClean,
@@ -47,36 +53,89 @@ const snippetRow = (snippet) => {
     
     snippetTitle.innerHTML = snippet.title;
     snippetDiv.className = "single__snippet";
+    snippetDiv.addEventListener("click", copySnippet);
     snippetDiv.appendChild(snippetTitle);
+    
+    let snippetKeywordsDiv = keyWordsDiv(snippet);
 
     if (snippet.keywords.length > 0) {
-        let snippetKeywordsDiv = keyWordsDiv(snippet);
         snippetDiv.appendChild(snippetKeywordsDiv);
     }
-    snippetDiv.appendChild(deleteButton());
+
+    snippetDiv.appendChild(actionContainer());   
     return snippetDiv;
 }
 
-const deleteButton = () => {
-    const deleteButton = document.createElement("button");
+const actionContainer = () => {
+    const actionContainer = document.createElement("div");
+    actionContainer.className = "snippet__action__container";
+
+    const deleteButton = document.createElement("span");
     deleteButton.innerHTML = "Delete";
     deleteButton.className = "snippet__delete__button";
-    // float the delete button to the right
-    deleteButton.style.float = "right";
-    // add the delete button to the snippet div
-    return deleteButton;
+    deleteButton.addEventListener("click", deleteSnippet);
+    
+    actionContainer.appendChild(deleteButton);
+
+
+    return actionContainer;
 }
 
 const keyWordsDiv = (snippet) => {
     const snippetKeywordsDiv = document.createElement("div");
     snippetKeywordsDiv.className = "snippet__keywords__container";
     for (let i = 0; i < snippet.keywords.length; i++) {
-        const keyword = document.createElement("span");
-        keyword.innerHTML = snippet.keywords[i];
-        keyword.className = "snippet__keyword";
-        snippetKeywordsDiv.appendChild(keyword);
+        const keyword = snippet.keywords[i];
+        if (keyword) {
+            const keywordDiv = document.createElement("div");
+            keywordDiv.className = "snippet__keyword";
+            keywordDiv.innerHTML = keyword;
+            snippetKeywordsDiv.appendChild(keywordDiv);
+        }
     }
     return snippetKeywordsDiv;
+}
+
+const copySnippet = (e) => {
+    const snippet = e.target.parentElement;
+    // only coppy the snippet if it has keywords
+    if (snippet.children[1].children.length > 0) {
+        const snippetText = snippet.children[0].innerHTML;
+        const snippetKeywords = snippet.children[1].innerHTML;
+        const snippetTextToCopy = snippetText + " " + snippetKeywords;
+        copyToClipboard(snippetTextToCopy);
+    }
+
+    // copy the snippet text to the clipboard
+    const copyToClipboard = (text) => {
+        const textArea = document.createElement("textarea");
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand("copy");
+        textArea.remove();
+    }
+
+    // display a message that the snippet was copied
+    const message = document.createElement("div");
+    message.className = "snippet__message";
+    message.innerHTML = "Snippet copied to clipboard";
+    snippet.appendChild(message);
+    setTimeout(() => {
+        snippet.removeChild(message);
+    }
+    , 2000);
+
+    
+
+}
+
+const deleteSnippet = (e) => {
+    const snippetDiv = e.target.parentElement.parentElement;
+    const snippetIndex = snippets.findIndex(snippet => snippet.title === snippetDiv.children[0].innerHTML);
+    snippets.splice(snippetIndex, 1);
+    saveSnippets();
+    displaySnippets();
 }
 
 
